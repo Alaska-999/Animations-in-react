2cb5e985caec123a106253375a78c814125ba5c6
Alaska-999 <kristinagaranchuk@gmail.com>
2022-06-25 15:58:03 +0300

    React Transition Group

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {Transition, CSSTransition, SwitchTransition} from "react-transition-group";
+import {Transition, CSSTransition, SwitchTransition, TransitionGroup} from "react-transition-group";
 
 
 function App() {
+    const [text, setText] = useState('')
 
-    const [mode, setMode] = useState('out-in')
-    const [toggle, setToggle] = useState(false)
+    const [todoList, setTodoList] = useState([
+        {id: 1, text: 'React'},
+        {id: 2, text: 'Java Script'}
+    ])
 
-    function changeHandler(e) {
-        setMode(e.target.value)
+    function addTodo() {
+        setTodoList([...todoList, {id: Date.now(), text}])
     }
 
     return (
         <div className="app">
-            <div className="wrap">
-                <label htmlFor='out-in'>out-in</label>
-                <input onChange={(e) => changeHandler(e)} id='out-in' value='out-in' name='radio' type="radio"/>
-                <label htmlFor='in-out'>in-out</label>
-                <input onChange={(e) => changeHandler(e)} id='in-out' value='in-out' name='radio' type="radio"/>
+            <div>
+                <input onChange={e => setText(e.target.value)} value={text} type="text"/>
+                <button onClick={() => addTodo()}>Add</button>
             </div>
-            <SwitchTransition mode={mode}>
-                <CSSTransition
-                    key={toggle}
-                    timeout={500}
-                    classNames='fade'
-                >
-                    <button onClick={() => setToggle(!toggle)}>
-                        {toggle ? 'Hello world!' : 'Goodbye world!'}
-                    </button>
-                </CSSTransition>
-            </SwitchTransition>
+            <TransitionGroup component='ul'>
+
+                    {todoList.map(({id, text}) =>
+                        <CSSTransition
+                            key={id}
+                            timeout = {500}
+                            classNames = 'todo'
+                        >
+                            <li className='todo'
+                                onClick={() => setTodoList([...todoList.filter(todo => todo.id !== id)])}>
+                                {id} {text}
+                            </li>
+                        </CSSTransition>
+                    )}
+
+            </TransitionGroup>
         </div>
     );
 }
